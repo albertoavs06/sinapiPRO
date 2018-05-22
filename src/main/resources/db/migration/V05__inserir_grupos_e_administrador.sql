25fa7b24d1d502d355f4bba31c5f5c9bf04cb45a
SergioPacheco <asfpacheco@yahoo.com.br>
2018-05-22 11:36:54 -0300

    flyway sql migration

--- src/main/resources/db/migration/V05__inserir_grupos_e_administrador.sql
+++ src/main/resources/db/migration/V05__inserir_grupos_e_administrador.sql
@@ -1,2 +1,7 @@
 INSERT INTO grupo (codigo, nome) VALUES (1, 'Administrador');
 INSERT INTO grupo (codigo, nome) VALUES (2, 'Vendedor');
+
+-- Hash = admin 
+-- $2a$10$g.wT4R0Wnfel1jc/k84OXuwZE02BlACSLfWy6TycGPvvEKvIm86SG
+
+INSERT INTO usuario (nome, email, senha, ativo) VALUES ('Admin', 'admin@sinapipro.com', '$2a$10$g.wT4R0Wnfel1jc/k84OXuwZE02BlACSLfWy6TycGPvvEKvIm86SG', 1)
